00f7ddd17c8119d5a5c3f7b9b142b65dc3a9a55c
Anton <coleman45rus@gmail.com>
2017-09-05 11:47:57 +0500

    Third commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 (function (window) {
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
   var myTruck = new Truck('Галактика', new DataStore());
   window.myTruck = myTruck; // экспорт экземпляра Truck
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
 })(window);
